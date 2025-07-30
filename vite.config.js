646557c157f7381fcafe4e750d06a908289928da
SukhpreetKaur2214 <164426432+SukhpreetKaur2214@users.noreply.github.com>
2025-07-29 21:40:26 -0400

    Update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/contact-book/',  // repo name as your base path
-  plugins: [vue()]
+  base: '/contact-book/', // Your GitHub repo name
+  plugins: [vue()],
 })
